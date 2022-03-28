854548ed18fa2363b3cb0746ad02206e53b2cd4c
pradeep-charism <pradeep.charism@gmail.com>
2022-03-28 20:14:59 +0800

    Build charts with moving average

--- yahoo-finance/build-stock-dataframes.py
+++ yahoo-finance/build-stock-dataframes.py
@@ -20,7 +20,7 @@ bottom.bar(df.index, df['Volume'])
 
 # set the labels
 top.axes.get_xaxis().set_visible(False)
-top.set_title('CapitalMall Trust')
+top.set_title('Apple')
 top.set_ylabel('Adj Closing Price')
 bottom.set_ylabel('Volume')
 
@@ -29,6 +29,6 @@ sma10 = df['Close'].rolling(10).mean()  # 10 days
 sma20 = df['Close'].rolling(20).mean()  # 20 days
 sma50 = df['Close'].rolling(50).mean()  # 50 days
 
-sma = pd.DataFrame({'CMT': df['Close'], 'SMA 10': sma10, 'SMA 20': sma20, 'SMA 50': sma50})
+sma = pd.DataFrame({'AAPL': df['Close'], 'SMA 10': sma10, 'SMA 20': sma20, 'SMA 50': sma50})
 sma.plot(figsize=(10, 5), legend=True, title='Apple')
 plt.show()
